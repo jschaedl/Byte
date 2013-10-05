3dba9f092e7cd8fa36dcc59efb9ca1e23974c907
Jan Schädlich <schaedlich.jan@gmail.com>
2013-10-05 15:00:05 +0200

    Refactored constant names

--- library/Byte/ByteConverter.php
+++ library/Byte/ByteConverter.php
@@ -3,39 +3,39 @@
 namespace Byte;
 
 class ByteConverter 
-{    
-    const Byte_String = 'b';
-    const K_Byte_String = 'k';
-    const M_Byte_String = 'm';
-    const G_Byte_String = 'g';
-    
-    protected static $units = array(
-        self::Byte_String => 1,
-        self::K_Byte_String => 1024,
-        self::M_Byte_String => 1048576,
-        self::G_Byte_String => 1073741824
-    );
-    
+{
+	const BYTE_STRING = 'b';
+	const K_BYTE_STRING = 'k';
+	const M_BYTE_STRING = 'm';
+	const G_BYTE_STRING = 'g';
+	
+	protected static $units = array (
+			self::BYTE_STRING => 1,
+			self::K_BYTE_STRING => 1024,
+			self::M_BYTE_STRING => 1048576,
+			self::G_BYTE_STRING => 1073741824 
+	);
+	
 	public function getBytes($input) {
-	    return $this->compute($input, self::Byte_String);
+		return $this->compute ( $input, self::BYTE_STRING );
 	}
 	
 	public function getKBytes($input) {
-	    return $this->compute($input, self::K_Byte_String);
+		return $this->compute ( $input, self::K_BYTE_STRING );
 	}
 	
 	public function getMBytes($input) {
-	    return $this->compute($input, self::M_Byte_String);
+		return $this->compute ( $input, self::M_BYTE_STRING );
 	}
 	
 	public function getGBytes($input) {
-	    return $this->compute($input, self::G_Byte_String);
+		return $this->compute ( $input, self::G_BYTE_STRING );
 	}
 	
 	private function compute($input, $unitOut) {
-	    $value = (int)substr($input, 0, -1);
-	    $unitIn = strtolower(substr($input, -1));
-	    return (isset(self::$units[$unitIn])) ?
-	       (($value * self::$units[$unitIn]) / self::$units[$unitOut]) : 0;
+		$value = ( int ) substr ( $input, 0, - 1 );
+		$unitIn = strtolower ( substr ( $input, - 1 ) );
+		return (isset ( self::$units [$unitIn] )) ? 
+			(($value * self::$units [$unitIn]) / self::$units [$unitOut]) : 0;
 	}
 }
--- tests/ByteConverterToByteTest.php
+++ tests/ByteConverterToByteTest.php
@@ -6,9 +6,9 @@ use Byte\ByteConverter;
 
 class ByteConverterToByteTest extends \PHPUnit_Framework_TestCase
 {	
-    const G_Bytes = 1073741824;
-    const M_Bytes = 1048576;
-    const K_Bytes = 1024;
+    const G_BYTES = 1073741824;
+    const M_BYTES = 1048576;
+    const K_BYTES = 1024;
     
     protected $converter;
     
@@ -21,20 +21,20 @@ class ByteConverterToByteTest extends \PHPUnit_Framework_TestCase
 	}
 	
 	public function testByteConvertGByteToByte() {
-		$this->assertEquals(self::G_Bytes, $this->converter->getBytes('1g'));
-		$this->assertEquals(5*self::G_Bytes, $this->converter->getBytes('5g'));
-		$this->assertEquals(10*self::G_Bytes, $this->converter->getBytes('10g'));
+		$this->assertEquals(self::G_BYTES, $this->converter->getBYTES('1g'));
+		$this->assertEquals(5*self::G_BYTES, $this->converter->getBYTES('5g'));
+		$this->assertEquals(10*self::G_BYTES, $this->converter->getBYTES('10g'));
 	}
 	
 	public function testByteConvertMByteToByte() {
-	    $this->assertEquals(self::M_Bytes, $this->converter->getBytes('1m'));
-	    $this->assertEquals(5*self::M_Bytes, $this->converter->getBytes('5m'));
-	    $this->assertEquals(10*self::M_Bytes, $this->converter->getBytes('10m'));
+	    $this->assertEquals(self::M_BYTES, $this->converter->getBYTES('1m'));
+	    $this->assertEquals(5*self::M_BYTES, $this->converter->getBYTES('5m'));
+	    $this->assertEquals(10*self::M_BYTES, $this->converter->getBYTES('10m'));
 	}
 	
 	public function testByteConvertKByteToByte() {
-	    $this->assertEquals(self::K_Bytes, $this->converter->getBytes('1k'));
-	    $this->assertEquals(5*self::K_Bytes, $this->converter->getBytes('5k'));
-	    $this->assertEquals(10*self::K_Bytes, $this->converter->getBytes('10k'));
+	    $this->assertEquals(self::K_BYTES, $this->converter->getBYTES('1k'));
+	    $this->assertEquals(5*self::K_BYTES, $this->converter->getBYTES('5k'));
+	    $this->assertEquals(10*self::K_BYTES, $this->converter->getBytes('10k'));
 	}
 }
